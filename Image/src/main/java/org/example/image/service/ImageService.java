b78e85350ba2627d77632b7696f396077ebadd93
ChunXiangWang <90006503+ChunXiangWang@users.noreply.github.com>
2021-12-30 17:32:00 -0500

    Delete Image directory

--- Image/src/main/java/org/example/image/service/ImageService.java
+++
@@ -1,7 +0,0 @@
-package org.example.image.service;
-
-import java.awt.image.BufferedImage;
-
-public interface ImageService {
-    boolean imageContainsCat(BufferedImage image, float confidenceThreshhold);
-}
